d6b9c400e97e1acdf86856575e096dc40b801e57
Marc Ribalta Gene <mariva1995@gmail.com>
2017-04-27 16:10:20 +0200

    starting to create the DataBase

--- CRMapp/models.py
+++ CRMapp/models.py
@@ -1,5 +1,40 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class WebUser(models.Model):
+    django_user = models.Model(User)
+    country = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    postal_code = models.IntegerField()
+    street = models.CharField(max_length=50)
+
+
+class UserAsPerson(models.Model):
+    web_user = models.Model(WebUser)
+
+
+class UserAsCompany(models.Model):
+    web_user = models.Model(WebUser)
+
+
+class WorkerUser(models.Model):
+    django_user = models.Model(User)
+
+class Opinion(models.Model):
+    user = models.ForeignKey(WebUser)
+    product = models.ForeignKey(Product)
+    name = models.CharField(max_length=30)
+
+
+class Incidence(models.Model):
+    user = models.ForeignKey(WebUser)
+    product = models.ForeignKey(Product)
+    name = models.CharField(max_length=30)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
