4879191e7047a8ea7d218d1de2c1b7f0dac13119
Nector12 <rarquesvall@gmail.com>
2017-04-28 16:53:24 +0200

    Added url, view and base html for the register page

--- CRMapp/views.py
+++ CRMapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        #  Registration process here
+        pass
