7766e80d6a6c9a44d3dbf8f420c4b63cf6c8caec
Marc Ribalta Gene <mariva1995@gmail.com>
2017-04-30 16:49:13 +0200

    Changed to sqllite temporally

--- CRM/settings.py
+++ CRM/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crmdatabase',
-        'USER': 'gt1',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
