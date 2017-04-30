80aac1a42def65c3756099dbc45c645a15234583
Marc Ribalta Gene <mariva1995@gmail.com>
2017-04-30 02:13:59 +0200

    Django database now works with postgresql, must install requirements.txt and read the instructions file

--- CRM/settings.py
+++ CRM/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crmdatabase',
+        'USER': 'gt1',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
