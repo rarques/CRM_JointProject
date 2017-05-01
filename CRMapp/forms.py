eaed1fef765d6b32f5bf85dfeb34e383ab582802
rarques <rarquesvall@gmail.com>
2017-05-01 20:50:00 +0200

    Views for register created. Logic not implemented

--- CRMapp/forms.py
+++ CRMapp/forms.py
@@ -1,5 +1,19 @@
+# coding=utf-8
+
 from django.forms import *
-from models import *
+
+
+class BasicForm(Form):
+    username = CharField(label='Usuari', max_length=30)
+    password = CharField(label='Contrassenya', max_length=50)
+    repassword = CharField(label='Confirmació contrassenya', max_length=50)
+    email = EmailField(label='E-mail', max_length=50)
+    phone = CharField(label='Telèfon', max_length=50)
+    street = CharField(label='Carrer', max_length=50)
+    city = CharField(label='Ciutat', max_length=30)
+    zipcode = CharField(label='Codi postal', max_length=10)
+    province = CharField(label='Província', max_length=30)
+    country = CharField(label='País', max_length=30)
 
 
 class UserAsPersonForm(Form):
--- CRMapp/views.py
+++ CRMapp/views.py
@@ -5,14 +5,15 @@ from forms import *
 # Create your views here.
 def register(request):
     return render_to_response('register.html', {
-        "title": "Register",
+        "title": "Register as a Person or as a Company",
     })
 
 
 def register_person(request):
     if request.method == 'GET':
-        return render_to_response('register.html', {
+        return render(request, 'register.html', {
             "title": "Register as Person",
+            "basic_form": BasicForm(),
             "form": UserAsPersonForm(),
         })
     elif request.method == 'POST':
@@ -22,11 +23,11 @@ def register_person(request):
 
 def register_company(request):
     if request.method == 'GET':
-        return render_to_response('register.html', {
+        return render(request, 'register.html', {
             "title": "Register as Company",
+            "basic_form": BasicForm(),
             "form": UserAsCompanyForm(),
         })
-
     elif request.method == 'POST':
         # Register company
         pass
