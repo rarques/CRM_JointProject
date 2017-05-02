d317bea824c894d03f1478ff8c7f61f6e0a14f57
cclauwers <cclauwers@gmail.com>
2017-05-02 22:14:13 +0200

    Login OK

--- CRM/urls.py
+++ CRM/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', login, name='login'),
+
 ]
