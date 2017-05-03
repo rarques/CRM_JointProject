92a067d03f23021c377a0b4c8e3464ad8a2fe4fc
cclauwers <cclauwers@gmail.com>
2017-05-03 09:10:28 +0200

    logout OK

--- CRM/urls.py
+++ CRM/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login,logout
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
 
 ]
