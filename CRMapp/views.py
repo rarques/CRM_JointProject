cae1ac0a55b12f42f46dabc8ae09c496e251d232
naluem <mdt_ps32468@hotmail.com>
2017-05-04 03:58:00 +0200

    fixed urls

--- CRMapp/views.py
+++ CRMapp/views.py
@@ -43,7 +43,7 @@ def company_profile(request):
         user = request.user
         web_user = WebUser.objects.get(django_user=user)
         user_as_company = UserAsCompany.objects.get(web_user=web_user)
-        return render_to_response(template_name='company_profile.html.html',
+        return render_to_response(template_name='company_profile.html',
                                   context={
                                       'user_name': user.username,
                                       'first_name': user.first_name,
@@ -65,15 +65,31 @@ def modify_person(request):
     Modifies the profile of a user of type person
     :param request: HttpRequest
     """
+    user = request.user
+    web_user = WebUser.objects.get(django_user=user)
+    user_as_person = UserAsPerson.objects.get(web_user=web_user)
     if request.method == 'POST' \
             and request.POST['password'] \
                     == request.POST['repeat_password']:
-        user = request.user
-        web_user = WebUser.objects.get(django_user=user)
-        user_as_person = UserAsPerson.objects.get(web_user=web_user)
-        parameters = get_person_profile_parameters(request)
+        parameters = get_person_profile_parameters(request.POST)
         update_person_profile(parameters, user, web_user, user_as_person)
-    redirect(to='person_profile.html')
+    else:
+        return render_to_response(template_name='modify_person.html',
+                                  context={
+                                      'user_name': user.username,
+                                      'first_name': user.first_name,
+                                      'last_name': user.last_name,
+                                      'email': user.email,
+                                      'country': web_user.country,
+                                      'province': web_user.province,
+                                      'city': web_user.province,
+                                      'zip_code': web_user.zip_code,
+                                      'street': web_user.street,
+                                      'phone': web_user.phone,
+                                      'dni': user_as_person.DNI
+                                  }
+                                  )
+    return redirect(to='person_profile.html')
 
 
 @login_required
@@ -82,105 +98,121 @@ def modify_company(request):
     Modifies the profile of a user of type company
     :param request: HttpRequest
     """
+    user = request.user
+    web_user = WebUser.objects.get(django_user=user)
+    user_as_company = UserAsCompany.objects.get(web_user=web_user)
     if request.method == 'POST' \
             and request.POST['password'] \
                     == request.POST['repeat_password']:
-        user = request.user
-        web_user = WebUser.objects.get(django_user=user)
-        user_as_company = UserAsCompany.objects.get(web_user=web_user)
-        parameters = get_company_profile_parameters(request)
+        parameters = get_company_profile_parameters(request.POST)
         update_company_profile(parameters, user, web_user, user_as_company)
-    redirect(to='company_profile.html')
+    else:
+        return render_to_response(template_name='modify_company.html',
+                                  context={
+                                      'user_name': user.username,
+                                      'first_name': user.first_name,
+                                      'last_name': user.last_name,
+                                      'email': user.email,
+                                      'country': web_user.country,
+                                      'province': web_user.province,
+                                      'city': web_user.province,
+                                      'zip_code': web_user.zip_code,
+                                      'street': web_user.street,
+                                      'phone': web_user.phone,
+                                      'cif': user_as_company.CIF
+                                  }
+                                  )
+    return redirect(to='company_profile.html')
 
 
-def get_company_profile_parameters(request):
+def get_company_profile_parameters(source):
     """
     Capture the parameters of the company type user profile
     :param request: HttpRequest
     :return: A dictionary with parameters
     """
     parameters = {}
-    get_basic_parameters(parameters, request)
-    get_user_parameters(parameters, request)
-    get_company_parameters(parameters, request)
+    get_basic_parameters(parameters, source)
+    get_user_parameters(parameters, source)
+    get_company_parameters(parameters, source)
     return parameters
 
 
-def get_person_profile_parameters(request):
+def get_person_profile_parameters(source):
     """
     Capture the parameters of the person type user profile
     :param request: HttpRequest
     :return: A dictionary with parameters
     """
     parameters = {}
-    get_basic_parameters(parameters, request)
-    get_user_parameters(parameters, request)
-    get_company_parameters(parameters, request)
+    get_basic_parameters(parameters, source)
+    get_user_parameters(parameters, source)
+    get_company_parameters(parameters, source)
     return parameters
 
 
-def get_basic_parameters(parameters, request):
+def get_basic_parameters(parameters, source):
     """
     Captures the parameters associated with the Django user model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # basic fields
-    parameters['user_name'] = request.POST['user_name'] \
-        if 'user_name' in request.POST['user_name'] else ''
-    parameters['first_name'] = request.POST['first_name'] \
-        if 'first_name' in request.POST['first_name'] else ''
-    parameters['last_name'] = request.POST['last_name'] \
-        if 'last_name' in request.POST['last_name'] else ''
-    parameters['email'] = request.POST['email'] \
-        if 'email' in request.POST['email'] else ''
-    parameters['password'] = request.POST['password'] \
-        if 'password' in request.POST['password'] else ''
-    parameters['repeat_password'] = request.POST['repeat_password'] \
-        if 'repeat_password' in request.POST['repeat_password'] else ''
+    parameters['user_name'] = source['user_name'] \
+        if 'user_name' in source['user_name'] else ''
+    parameters['first_name'] = source['first_name'] \
+        if 'first_name' in source['first_name'] else ''
+    parameters['last_name'] = source['last_name'] \
+        if 'last_name' in source['last_name'] else ''
+    parameters['email'] = source['email'] \
+        if 'email' in source['email'] else ''
+    parameters['password'] = source['password'] \
+        if 'password' in source['password'] else ''
+    parameters['repeat_password'] = source['repeat_password'] \
+        if 'repeat_password' in source['repeat_password'] else ''
 
 
-def get_user_parameters(parameters, request):
+def get_user_parameters(parameters, source):
     """
     Captures the parameters associated with the WebUser model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # user information
-    parameters['country'] = request.POST['country'] \
-        if 'country' in request.POST['country'] else ''
-    parameters['province'] = request.POST['province'] \
-        if 'province' in request.POST['province'] else ''
-    parameters['city'] = request.POST['city'] \
-        if 'city' in request.POST['city'] else ''
-    parameters['zip_code'] = request.POST['zip_code'] \
-        if 'zip_code' in request.POST['zip_code'] else ''
-    parameters['street'] = request.POST['street'] \
-        if 'street' in request.POST['street'] else ''
-    parameters['phone'] = request.POST['phone'] \
-        if 'phone' in request.POST['phone'] else ''
+    parameters['country'] = source['country'] \
+        if 'country' in source['country'] else ''
+    parameters['province'] = source['province'] \
+        if 'province' in source['province'] else ''
+    parameters['city'] = source['city'] \
+        if 'city' in source['city'] else ''
+    parameters['zip_code'] = source['zip_code'] \
+        if 'zip_code' in source['zip_code'] else ''
+    parameters['street'] = source['street'] \
+        if 'street' in source['street'] else ''
+    parameters['phone'] = source['phone'] \
+        if 'phone' in source['phone'] else ''
 
 
-def get_company_parameters(parameters, request):
+def get_company_parameters(parameters, source):
     """
     Captures the parameters associated with the UserAsCompany model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # company information
-    parameters['cif'] = request.POST['cif'] \
-        if 'cif' in request.POST['cif'] else ''
+    parameters['cif'] = source['cif'] \
+        if 'cif' in source['cif'] else ''
 
 
-def get_person_parameters(parameters, request):
+def get_person_parameters(parameters, source):
     """
     Captures the parameters associated with the UserAsPerson model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # company information
-    parameters['dni'] = request.POST['dni'] \
-        if 'dni' in request.POST['dni'] else ''
+    parameters['dni'] = source['dni'] \
+        if 'dni' in source['dni'] else ''
 
 
 def update_company_profile(parameters, user, web_user, user_as_company):
