3ef1840dc14a19a0d126bca5ac901083e7ff9b1e
naluem <mdt_ps32468@hotmail.com>
2017-05-04 06:02:14 +0200

    modify person and company finished

--- CRMapp/views.py
+++ CRMapp/views.py
@@ -70,10 +70,10 @@ def modify_person(request):
     user = request.user
     web_user = WebUser.objects.get(django_user=user)
     user_as_person = UserAsPerson.objects.get(web_user=web_user)
-    if request.method == 'POST' \
-            and request.POST['password'] \
-                    == request.POST['repeat_password']:
-        parameters = get_person_profile_parameters(request.POST)
+    if request.method == 'POST':
+        parameters = get_person_profile_parameters(request.POST,
+                                                   user, web_user,
+                                                   user_as_person)
         update_person_profile(parameters, user, web_user, user_as_person)
     else:
         return render(request,
@@ -104,11 +104,11 @@ def modify_company(request):
     user = request.user
     web_user = WebUser.objects.get(django_user=user)
     user_as_company = UserAsCompany.objects.get(web_user=web_user)
-    if request.method == 'POST' \
-            and request.POST['password'] \
-                    == request.POST['repeat_password']:
-        parameters = get_company_profile_parameters(request.POST)
-        update_company_profile(parameters, user, web_user, user_as_company)
+    if request.method == 'POST':
+        parameters = get_company_profile_parameters(request.POST,
+                                                    user, web_user, user_as_company)
+        update_company_profile(parameters, user, web_user,
+                               user_as_company)
     else:
         return render(request,
                       'modify_company.html',
@@ -129,94 +129,77 @@ def modify_company(request):
     return redirect(to='../company_profile')
 
 
-def get_company_profile_parameters(source):
+def get_company_profile_parameters(source, user, web_user, user_as_company):
     """
     Capture the parameters of the company type user profile
     :param request: HttpRequest
     :return: A dictionary with parameters
     """
     parameters = {}
-    get_basic_parameters(parameters, source)
-    get_user_parameters(parameters, source)
-    get_company_parameters(parameters, source)
+    get_basic_parameters(parameters, source, user)
+    get_user_parameters(parameters, source, web_user)
+    get_company_parameters(parameters, source, user_as_company)
     return parameters
 
 
-def get_person_profile_parameters(source):
+def get_person_profile_parameters(source, user, web_user, user_as_person):
     """
     Capture the parameters of the person type user profile
     :param request: HttpRequest
     :return: A dictionary with parameters
     """
     parameters = {}
-    get_basic_parameters(parameters, source)
-    get_user_parameters(parameters, source)
-    get_person_parameters(parameters, source)
+    get_basic_parameters(parameters, source, user)
+    get_user_parameters(parameters, source, web_user)
+    get_person_parameters(parameters, source, user_as_person)
     return parameters
 
 
-def get_basic_parameters(parameters, source):
+def get_basic_parameters(parameters, source, user):
     """
     Captures the parameters associated with the Django user model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # basic fields
-    parameters['user_name'] = source['user_name'] \
-        if 'user_name' in source['user_name'] else ''
-    parameters['first_name'] = source['first_name'] \
-        if 'first_name' in source['first_name'] else ''
-    parameters['last_name'] = source['last_name'] \
-        if 'last_name' in source['last_name'] else ''
-    parameters['email'] = source['email'] \
-        if 'email' in source['email'] else ''
-    parameters['password'] = source['password'] \
-        if 'password' in source['password'] else ''
-    parameters['repeat_password'] = source['repeat_password'] \
-        if 'repeat_password' in source['repeat_password'] else ''
+    parameters['user_name'] = source['user_name']
+    parameters['first_name'] = source['first_name']
+    parameters['last_name'] = source['last_name']
+    parameters['email'] = source['email']
 
-
-def get_user_parameters(parameters, source):
+def get_user_parameters(parameters, source, web_user):
     """
     Captures the parameters associated with the WebUser model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # user information
-    parameters['country'] = source['country'] \
-        if 'country' in source['country'] else ''
-    parameters['province'] = source['province'] \
-        if 'province' in source['province'] else ''
-    parameters['city'] = source['city'] \
-        if 'city' in source['city'] else ''
-    parameters['zip_code'] = source['zip_code'] \
-        if 'zip_code' in source['zip_code'] else ''
-    parameters['street'] = source['street'] \
-        if 'street' in source['street'] else ''
-    parameters['phone'] = source['phone'] \
-        if 'phone' in source['phone'] else ''
+    parameters['country'] = source['country']
+    parameters['province'] = source['province']
+    parameters['city'] = source['city']
+    parameters['zip_code'] = source['zip_code']
+    parameters['street'] = source['street']
+    parameters['phone'] = source['phone']
 
 
-def get_company_parameters(parameters, source):
+def get_company_parameters(parameters, source, user_as_company):
     """
     Captures the parameters associated with the UserAsCompany model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # company information
-    parameters['cif'] = source['cif'] \
-        if 'cif' in source['cif'] else ''
+    parameters['cif'] = source['cif']
 
 
-def get_person_parameters(parameters, source):
+def get_person_parameters(parameters, source, user_as_person):
     """
     Captures the parameters associated with the UserAsPerson model
     :param parameters: The dictionary where the parameters will be stored
     :param request: HttpRequest
     """
     # company information
-    parameters['dni'] = source['dni'] \
-        if 'dni' in source['dni'] else ''
+    parameters['dni'] = source['dni']
 
 
 def update_company_profile(parameters, user, web_user, user_as_company):
@@ -230,9 +213,11 @@ def update_company_profile(parameters, user, web_user, user_as_company):
     update_basic_parameters(parameters, user)
     update_user_parameters(parameters, web_user)
     update_company_parameters(parameters, user_as_company)
-    """user.save(force_update=True)
-    web_user.save(force_update=True)
-    user_as_company.save(force_update=True)"""
+    user.save(update_fields=["username", "first_name", "last_name",
+                             "email"])
+    web_user.save(update_fields=["country", "province", "city", "zip_code",
+                                 "street", "phone"])
+    user_as_company.save(update_fields=["CIF"])
 
 
 def update_person_profile(parameters, user, web_user, user_as_person):
@@ -246,9 +231,11 @@ def update_person_profile(parameters, user, web_user, user_as_person):
     update_basic_parameters(parameters, user)
     update_user_parameters(parameters, web_user)
     update_person_parameters(parameters, user_as_person)
-    """user.save(force_update=True)
-    web_user.save(force_update=True)
-    user_as_person.save(force_update=True)"""
+    user.save(update_fields=["username", "first_name", "last_name",
+                             "email"])
+    web_user.save(update_fields=["country", "province", "city", "zip_code",
+                                 "street", "phone"])
+    user_as_person.save(update_fields=["DNI"])
 
 
 def update_basic_parameters(parameters, user):
@@ -257,12 +244,10 @@ def update_basic_parameters(parameters, user):
     :param parameters: Dictionary that contains all the parameters
     :param user: Django user model
     """
-    user.username=parameters['user_name']
-    user.first_name=parameters['first_name']
-    user.last_name=parameters['last_name']
-    user.email=parameters['email']
-    user.password=parameters['password']
-
+    user.username = parameters['user_name']
+    user.first_name = parameters['first_name']
+    user.last_name = parameters['last_name']
+    user.email = parameters['email']
 
 def update_user_parameters(parameters, web_user):
     """
@@ -270,12 +255,12 @@ def update_user_parameters(parameters, web_user):
     :param parameters: Dictionary that contains all the parameters
     :param web_user: WebUser model
     """
-    web_user.country=parameters['country']
-    web_user.province=parameters['province']
-    web_user.city=parameters['city']
-    web_user.zip_code=parameters['zip_code']
-    web_user.street=parameters['street']
-    web_user.phone=parameters['phone']
+    web_user.country = parameters['country']
+    web_user.province = parameters['province']
+    web_user.city = parameters['city']
+    web_user.zip_code = parameters['zip_code']
+    web_user.street = parameters['street']
+    web_user.phone = parameters['phone']
 
 
 def update_company_parameters(parameters, user_as_company):
@@ -284,7 +269,7 @@ def update_company_parameters(parameters, user_as_company):
     :param parameters: Dictionary that contains all the parameters
     :param user_as_company: UserAsCompany model
     """
-    user_as_company.CIF=parameters['cif']
+    user_as_company.CIF = parameters['cif']
 
 
 def update_person_parameters(parameters, user_as_person):
@@ -293,4 +278,4 @@ def update_person_parameters(parameters, user_as_person):
     :param parameters: Dictionary that contains all the parameters
     :param user_as_person: UserAsPerson model
     """
-    user_as_person.DNI=parameters['dni']
+    user_as_person.DNI = parameters['dni']
