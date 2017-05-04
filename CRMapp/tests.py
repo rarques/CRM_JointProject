005fc9fbb71adc0b1de84f0188cb304a01ea9438
Marc Ribalta Gene <mariva1995@gmail.com>
2017-05-04 15:17:41 +0200

    Added test between the discount and the product

--- CRMapp/tests.py
+++ CRMapp/tests.py
@@ -39,28 +39,24 @@ class ModelsTesting(TestCase):
         Employee.objects.create(django_user=user3, NSS="123N", department="marqueting")
 
     def test_person_attributes(self):
-        """Testing if correct attributes are given when a person is searched"""
         user = User.objects.get(username="user1")
         web_user = WebUser.objects.get(django_user=user)
         person = UserAsPerson.objects.get(web_user=web_user)
         self.assertEqual(person.DNI, "312W")
 
     def test_company_attributes(self):
-        """Testing if correct attributes are given when a company is searched"""
         user = User.objects.get(username="user2")
         web_user = WebUser.objects.get(django_user=user)
         company = UserAsCompany.objects.get(web_user=web_user)
         self.assertEqual(company.CIF, "12w2")
 
     def test_employee_attributes(self):
-        """Testing if correct attributes are given when an employee is searched"""
         user = User.objects.get(username="user3")
         employee = Employee.objects.get(django_user=user)
         self.assertEqual(employee.NSS, "123N")
         self.assertEquals(employee.department, "marqueting")
 
     def test_person_opinion(self):
-        """Person opinion about a product"""
         person = UserAsPerson.objects.get(DNI="312W")
         product = Product.objects.get(name="croissant")
         Opinion.objects.create(user=person.web_user, product=product, name="maravilla", comment="roto2", rating=5)
@@ -68,7 +64,6 @@ class ModelsTesting(TestCase):
         self.assertEqual(opinion.rating, 5)
 
     def test_company_opinion(self):
-        """Company opinion about a product"""
         company = UserAsCompany.objects.get(CIF="12w2")
         product = Product.objects.get(name="croissant")
         Opinion.objects.create(user=company.web_user, product=product, name="maravilla", comment="roto2", rating=4)
@@ -76,7 +71,6 @@ class ModelsTesting(TestCase):
         self.assertEqual(opinion.rating, 4)
 
     def test_person_incidence(self):
-        """Testing person incidence and getting it using the category"""
         person = UserAsPerson.objects.get(DNI="312W")
         product = Product.objects.get(name="croissant")
         Incidence.objects.create(user=person.web_user, product=product, name="que collons", explanation="lulz",
@@ -104,15 +98,24 @@ class ModelsTesting(TestCase):
         self.assertEqual(catched_user.user.phone, 288)
 
     def test_interested_users_in_category(self):
-        category = Category.objects.create(name="phone")
-        user1 = WebUser.objects.get(django_user=User.objects.get(username="user1"))
-        user2 = WebUser.objects.get(django_user=User.objects.get(username="user2"))
-        CategoryPerUser.objects.create(user=user1, category=category)
-        CategoryPerUser.objects.create(user=user2, category=category)
+        category, user1, user2 = self.creating_category_per_user()
         interested_users = CategoryPerUser.objects.filter(category=category)
         first_user = interested_users.get(user=user1)
         second_user = interested_users.get(user=user2)
         self.assertEqual(first_user.user.phone, 288)
         self.assertEqual(second_user.user.phone, 322)
 
-    """Test selling a product"""
+    def creating_category_per_user(self):
+        category = Category.objects.create(name="phone")
+        user1 = WebUser.objects.get(django_user=User.objects.get(username="user1"))
+        user2 = WebUser.objects.get(django_user=User.objects.get(username="user2"))
+        CategoryPerUser.objects.create(user=user1, category=category)
+        CategoryPerUser.objects.create(user=user2, category=category)
+        return category, user1, user2
+
+    def test_discount_on_product(self):
+        testing_product = Product.objects.get(name="croissant")
+        self.assertEqual(testing_product.discount.discount_identifier, "XX2D-AGS1")
+
+
+    """Starting the controller unit testing"""
