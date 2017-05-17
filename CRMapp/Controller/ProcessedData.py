9cc5f5f253eb9095786f8369fe992bc1d3fdc570
Marc Ribalta Gene <mariva1995@gmail.com>
2017-05-17 23:55:53 +0200

    top products added

--- CRMapp/Controller/ProcessedData.py
+++ CRMapp/Controller/ProcessedData.py
@@ -1,4 +1,4 @@
-from CRMapp.models import WebUser, Sale
+from CRMapp.models import WebUser, Sale, Product
 
 
 class ProcessedData():
@@ -17,3 +17,18 @@ class ProcessedData():
                 top_buyers_register.append(number_of_sales)
 
         return top_buyers
+
+    def get_top_products(self):
+        top_products = ["No product"] * self.NUMBER_OF_TOP
+        top_products_register = [0] * self.NUMBER_OF_TOP
+        for product in Product.objects.all():
+            number_of_sales = len(Sale.objects.filter(product=product))
+            if number_of_sales > min(top_products_register):
+                index_to_modify = top_products_register.index(min(top_products_register))
+                top_products.pop(index_to_modify)
+                top_products_register.pop(index_to_modify)
+                top_products.append(product.name)
+                top_products_register.append(number_of_sales)
+
+        return top_products
+
