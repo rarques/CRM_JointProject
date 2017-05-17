af091e9da12a50a8dae641ce968ac1881e5b7601
Marc Ribalta Gene <mariva1995@gmail.com>
2017-05-18 00:21:33 +0200

    All working

--- CRMapp/Controller/ProcessedData.py
+++ CRMapp/Controller/ProcessedData.py
@@ -32,3 +32,19 @@ class ProcessedData():
 
         return top_products
 
+    def get_bot_products(self):
+        bot_products = []
+        bot_products_register = []
+        for product in Product.objects.all():
+            number_of_sales = len(Sale.objects.filter(product=product))
+            if len(bot_products_register)<self.NUMBER_OF_TOP:
+                bot_products.append(product.name)
+                bot_products_register.append(number_of_sales)
+            elif number_of_sales < max(bot_products_register):
+                index_to_modify = bot_products_register.index(max(bot_products_register))
+                bot_products.pop(index_to_modify)
+                bot_products_register.pop(index_to_modify)
+                bot_products.append(product.name)
+                bot_products_register.append(number_of_sales)
+
+        return bot_products
