096aaa71d80063620b63abb165f542830e90dc2d
rarques <rarquesvall@gmail.com>
2017-05-18 17:19:21 +0200

    Redirect to profile, not modify profile

--- CRMapp/views.py
+++ CRMapp/views.py
@@ -306,6 +306,6 @@ def register_incidence(request, pk):
 def profile(request):
     web_user = WebUser.objects.filter(django_user=request.user)
     if UserAsPerson.objects.filter(web_user=web_user).exists():
-        return redirect(to='../../modify_person/')
+        return redirect(to='../../person_profile/')
     elif UserAsCompany.objects.filter(web_user=web_user).exists():
-        return redirect(to='../../modify_company/')
+        return redirect(to='../../company_profile/')
